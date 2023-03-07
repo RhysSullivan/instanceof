f524f48a7fef571882c7170cc4b80834ec99d931
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-03-07 00:00:10 -0500

    succeeding with yarn

--- apps/instanceof-checker/dist/index.js
+++ apps/instanceof-checker/dist/index.js
@@ -1,4 +1,26 @@
 "use strict";
-exports.__esModule = true;
-var instanceof_1 = require("instanceof");
-console.log((0, instanceof_1.isInstanceOf)());
+var __defProp = Object.defineProperty;
+var __name = (target, value) => __defProp(target, "name", { value, configurable: true });
+
+// ../../packages/instanceof/index.ts
+var Node = class {
+};
+__name(Node, "Node");
+var Text = class extends Node {
+};
+__name(Text, "Text");
+function isInstanceOf() {
+  const node = new Node();
+  const text = new Text();
+  console.log("Text instanceof Node", text instanceof Node);
+}
+__name(isInstanceOf, "isInstanceOf");
+
+// index.ts
+isInstanceOf();
+isInstanceOf();
+isInstanceOf();
+isInstanceOf();
+isInstanceOf();
+isInstanceOf();
+//# sourceMappingURL=index.js.map
